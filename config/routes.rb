577cba087a707f78a17ed04aeb3fb722b02f6366
Samantha Carp <samantha.carp@kellogg.northwestern.edu>
2024-05-17 20:21:28 +0000

    friday try

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+ # resources "places"
+
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  
+get root "places", to: "places#index"
+
 end
+
+ 
+  # get("/", { :controller => "articles", :action => "index" })
+
+
+get("/places", :controller => "places", :action => "index") 
+
+
+
