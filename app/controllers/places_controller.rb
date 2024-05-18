79ba2d990d572f637a3ba93579d8eb776b2ae07c
Samantha Carp <samantha.carp@kellogg.northwestern.edu>
2024-05-18 17:12:14 +0000

    sat AM edits

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,27 @@
-index
+class PlacesController < ApplicationController
+
+def index
+  @places = Place.all
+end
+
+def show
+  @places = Place.find_by({ "id" = params["id"]})
+  @entries = Contact.where({ "places_id" => @places ["id"]})
+
+end
 
 
+  def new
+  @place = Place.find_by({ "id" => params ["place_id"] }) 
+end
 
+def create
+  @entry = Entry.new
 
+@places.save
+
+redirect_to "/places"
+
+end
+end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
- # resources "places"
+resources "places"
+resources "entries"
 
   # Defines the root path route ("/")
-  
-get root "places", to: "places#index"
-
-end
-
- 
   # get("/", { :controller => "articles", :action => "index" })
-
-
-get("/places", :controller => "places", :action => "index") 
-
-
-
+end
